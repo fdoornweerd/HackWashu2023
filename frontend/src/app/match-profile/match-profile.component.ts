10c7847e095710caa8be861f12e35dc1effc25f7
LukeStock22 <l.d.stockbridge@wustl.edu>
2023-10-14 15:37:02 -0500

    Added Match-profile component

---
+++ frontend/src/app/match-profile/match-profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-match-profile',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './match-profile.component.html',
+  styleUrls: ['./match-profile.component.scss']
+})
+export class MatchProfileComponent {
+
+}
--- frontend/src/app/matches/matches.component.ts
+++ frontend/src/app/matches/matches.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-matches',
