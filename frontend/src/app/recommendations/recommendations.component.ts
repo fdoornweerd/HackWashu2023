aea5a528ec32792bff2a0364b686ec782fb81081
sylviakozub <k.sylvia@wustl.edu>
2023-10-14 15:14:17 -0500

    s

---
+++ frontend/src/app/recommendations/recommendations.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recommendations',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recommendations.component.html',
+  styleUrls: ['./recommendations.component.scss']
+})
+export class RecommendationsComponent {
+
+}
