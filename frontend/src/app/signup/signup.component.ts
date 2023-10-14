a74b20527b1b36b73d0ed4ab46184c4b22e3a103
finndoornweerd <dfinn@wustl.edu>
2023-10-14 14:05:28 -0500

    signup

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
 
+  name: string = '';
+  email: string = '';
+  password: string= '';
+
+  onSubmit() {
+    // Handle sign-in logic here (e.g., send a request to an authentication service).
+  }
 }
