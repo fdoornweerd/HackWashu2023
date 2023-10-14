2503aa38e30a9784b319ea1be82a0f65027de399
DovSchwartz <schwartzdov75@gmai.com>
2023-10-14 15:10:11 -0500

    resolve

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,22 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Replace with your home component
 import { SigninComponent } from './signin/signin.component'; // Replace with your sign-in component
-<<<<<<< HEAD
-=======
 import { HubComponent } from './hub/hub.component';
 import { SignupComponent } from './signup/signup.component';
->>>>>>> 0403ab940074c264f0d31fa4f888c7d5557bd8f5
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-<<<<<<< HEAD
-  { path: 'signin', component: SigninComponent }
-=======
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'hub', component: HubComponent}
->>>>>>> 0403ab940074c264f0d31fa4f888c7d5557bd8f5
 ];
 
 @NgModule({
