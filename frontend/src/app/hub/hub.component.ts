0403ab940074c264f0d31fa4f888c7d5557bd8f5
finndoornweerd <dfinn@wustl.edu>
2023-10-14 15:01:23 -0500

    Merge

---
+++ frontend/src/app/hub/hub.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hub',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hub.component.html',
+  styleUrls: ['./hub.component.scss']
+})
+export class HubComponent {
+
+}
