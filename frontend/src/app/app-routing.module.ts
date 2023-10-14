c6fa735d8304a68ae3b3ef622e90a541060f54e3
DovSchwartz <schwartzdov75@gmai.com>
2023-10-14 11:02:34 -0500

    set home component as starting page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Use an empty path to route to HomeComponent
+  // Define other routes as needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
