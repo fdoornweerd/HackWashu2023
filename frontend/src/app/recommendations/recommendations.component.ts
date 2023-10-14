d944472ccac391b65f6fc46553ad3f01e8f26f8f
LukeStock22 <l.d.stockbridge@wustl.edu>
2023-10-14 16:01:22 -0500

    Recommendations update

--- frontend/src/app/recommendations/recommendations.component.ts
+++ frontend/src/app/recommendations/recommendations.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recommendations',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './recommendations.component.html',
   styleUrls: ['./recommendations.component.scss']
 })
 export class RecommendationsComponent {
 
 }
+
+/*import { PersonService } from './person.service'; // Import your service
+
+@Component({
+  selector: 'app-person-list',
+  templateUrl: './person-list.component.html',
+  styleUrls: ['./person-list.component.css']
+})
+export class PersonListComponent implements OnInit {
+  people: any[] = [];
+
+  constructor(private personService: PersonService) {} // Inject your service
+
+  ngOnInit() {
+    this.personService.getPeople().subscribe((data: any[]) => {
+      this.people = data; // Populate the people array with data from your service
+    });
+  }
+}
+*/
+
+
+
+
+
