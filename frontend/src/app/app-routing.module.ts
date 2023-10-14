05d687dc20ac9e23f8107d72bf437377b9aa84bf
finndoornweerd <dfinn@wustl.edu>
2023-10-14 14:57:40 -0500

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Replace with your home component
 import { SigninComponent } from './signin/signin.component'; // Replace with your sign-in component
 import { HubComponent } from './hub/hub.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'hub', component: HubComponent}
 ];
 
