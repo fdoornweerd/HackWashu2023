2674c697a0caf2949855af4cc015fbc5f4a24815
DovSchwartz <schwartzdov75@gmai.com>
2023-10-14 15:07:25 -0500

    signup routs

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -14,9 +15,31 @@ export class SignupComponent {
 
   name: string = '';
   email: string = '';
-  password: string= '';
+  password: string = '';
+
+  constructor(private authService: AuthService) { }
 
   onSubmit() {
-    // Handle sign-in logic here (e.g., send a request to an authentication service).
+    this.registerUser();
+  }
+
+  registerUser() {
+    const userData = {
+      email: this.email,
+      password: this.password,
+      username: this.name,
+      // Add other user registration data fields
+    };
+
+    this.authService.registerUser(userData).subscribe({
+      next: (response) => {
+        console.log('User registered successfully', response);
+        // Handle success, e.g., navigate to a different page or display a success message
+      },
+      error: (error) => {
+        console.error('User registration failed', error);
+        // Handle error, e.g., display an error message to the user
+      }
+    });
   }
 }
