cad13196c5849044c57027682293a60c9035c1a1
finndoornweerd <dfinn@wustl.edu>
2023-10-14 17:46:10 -0500

    push

---
+++ frontend/src/app/Person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    proficient: string[];
+    learning: string[];
+    interests: string[];
+    bio: string;
+  }
