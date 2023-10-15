4ebb57ad08a1465908e02510507bca2d54c511e0
LukeStock22 <l.d.stockbridge@wustl.edu>
2023-10-14 23:02:10 -0500

    please work

---
+++ frontend/src/app/editprofiles/editprofiles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-editprofiles',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './editprofiles.component.html',
+  styleUrls: ['./editprofiles.component.scss']
+})
+export class EditprofilesComponent {
+
+}
