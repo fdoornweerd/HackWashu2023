9b024a2b150d28534cef8a72857c9b4672204dc9
sylviakozub <k.sylvia@wustl.edu>
2023-10-14 21:05:16 -0500

    tt

--- frontend/src/app/hub/hub.component.ts
+++ frontend/src/app/hub/hub.component.ts
@@ -3,25 +3,19 @@ import { CommonModule } from '@angular/common';
 import { PeopleContainerComponent } from '../people-container/people-container.component';
 import { RecommendContainerComponent } from '../recommend-container/recommend-container.component';
 import { AiChatComponent } from '../ai-chat/ai-chat.component';
-import { Router } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+
 
 @Component({
   selector: 'app-hub',
   standalone: true,
-  imports: [CommonModule,PeopleContainerComponent, RecommendContainerComponent, AiChatComponent],
+  imports: [CommonModule,PeopleContainerComponent, RecommendContainerComponent, AiChatComponent, NavbarComponent],
   templateUrl: './hub.component.html',
   styleUrls: ['./hub.component.scss']
 })
 
 
 export class HubComponent {
-  constructor(private router: Router) { }
-
-  signOut() {
-    // Add any sign-out logic here (e.g., clearing user session)
-    
-    // Navigate to the home page
-    this.router.navigate(['/']);
-
-}
+  
 }
