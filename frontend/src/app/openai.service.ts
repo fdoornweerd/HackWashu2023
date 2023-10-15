2a2650a615347223cfd0df8fc9e8e0d1106f0db9
finndoornweerd <dfinn@wustl.edu>
2023-10-15 11:14:19 -0500

    minor

--- frontend/src/app/openai.service.ts
+++ frontend/src/app/openai.service.ts
@@ -6,13 +6,13 @@ import { Message } from './Message';
   providedIn: 'root'
 })
 export class OpenaiService {
-  private apiKey = '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'; // Replace with your API key
-  private apiUrl = 'https://api.openai.com/v1/chat/completions'; // Replace with the OpenAI API endpoint
+  private apiKey = '';
+  private apiUrl = 'https://api.openai.com/v1/chat/completions'; 
   
-  // 'https://api.openai.com/v1/engines/gpt-3.5-turbo/completions'
+ 
   constructor(private http: HttpClient) {}
 
-  // Define a method to make requests to the OpenAI API
+
   getOpenAIResponse(prompts: Message[]): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
